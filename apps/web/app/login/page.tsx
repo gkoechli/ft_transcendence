b497b3314d6895df6c4eea10c14f2bd817e223fe
ibadus <ibadus@icloud.com>
2023-12-21 22:08:09 +0100

    added login page

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -3,7 +3,8 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { Gradient } from "../../components/gradient/gradient";
 import { useEffect } from "react";
-
+import { Si42 } from "react-icons/si";
+import { Button } from "@web/components/ui/button";
 
 export default function Home() {
 	useEffect(() => {
@@ -29,17 +30,33 @@ export default function Home() {
 					</filter>
 					<rect width="100%" height="100%" filter="url(#noise)"></rect>
 				</svg>
-				<main className="flex flex-col justify-center h-[90%] static md:fixed w-screen overflow-hidden grid-rows-[1fr_repeat(3,auto)_1fr] z-[100] pt-[30px] pb-[320px] px-4 md:px-20 md:py-0">
+				<main className="flex flex-col justify-center h-[90%] fixed w-screen overflow-hidden z-[100] pt-[30px] pb-[320px] px-4 md:px-20 md:py-0">
 
+					<motion.div
+						initial={{ opacity: 0, y: 40 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{
+							delay: 0.15,
+							duration: 0.95,
+							ease: [0.165, 0.84, 0.44, 1],
+						}}
+						className="w-full"
+					>
+						<h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight">
+							Sign in to your account
+						</h2>
+						<Button className="max-w-[32rem] w-full">
+							<Si42 className="w-4 h-4" />
+						</Button>
+					</motion.div>
 				</main>
 				<div
-					className="fixed top-0 right-0 w-[80%] md:w-1/2 h-screen bg-[#1F2B3A]/20"
+					className="fixed top-0 right-0 w-[80%] md:w-1/2 h-screen bg-[#1F2B3A]/20 max-md:hidden"
 					style={{
 						clipPath:
 							"polygon(100px 0,100% 0,calc(100% + 225px) 100%, 480px 100%)",
 					}}
 				></div>
-
 				<motion.canvas
 					initial={{
 						filter: "blur(20px)",
@@ -57,7 +74,7 @@ export default function Home() {
 					}}
 					id="gradient-canvas"
 					data-transition-in
-					className="z-50 fixed top-0 right-[-2px] w-[80%] md:w-1/2 h-screen bg-gray-950"
+					className="z-50 fixed top-0 right-[-2px] w-[80%] md:w-1/2 h-screen bg-gray-950 max-md:hidden"
 				></motion.canvas>
 			</div>
 		</AnimatePresence>

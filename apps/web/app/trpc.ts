6805048db26a788cc8cce81dea28a2db9b96d860
ibadus <ibadus@icloud.com>
2023-12-21 18:45:48 +0100

    added env checker on web

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -1,10 +1,11 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { AppRouter } from "@server/trpc/trpc.router";
+import { env } from "@web/env";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
+      url: `${env.NEXT_PUBLIC_NESTJS_SERVER}/trpc`,
     }),
   ],
 });

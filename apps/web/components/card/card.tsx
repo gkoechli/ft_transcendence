0fd4307d5e97bd40cb3ac80f4498dedef99f8135
ibadus <ibadus@icloud.com>
2023-12-25 19:09:50 +0100

    added className props

--- apps/web/components/card/card.tsx
+++ apps/web/components/card/card.tsx
@@ -1,8 +1,11 @@
-export function Card({ children }: {
+import { cn } from "@web/utils/utils"
+
+export function Card({ children, className }: {
 	children: React.ReactNode
+	className?: string
 }) {
 	return (
-		<section className="my-8 max-w-5xl">
+		<section className={cn("my-8 max-w-5xl", className)}>
 			<div className="bg-gray-800/50 rounded-md shadow overflow-hidden border-[1px] border-gray-700">
 				{children}
 			</div>

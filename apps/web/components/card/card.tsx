d7f13ee849877b4947ccefb5500b4ea8fa389e55
ibadus <ibadus@icloud.com>
2023-12-25 02:51:25 +0100

    changed mb-8 to my-8

--- apps/web/components/card/card.tsx
+++ apps/web/components/card/card.tsx
@@ -2,7 +2,7 @@ export function Card({ children }: {
 	children: React.ReactNode
 }) {
 	return (
-		<section className="mb-8 max-w-5xl">
+		<section className="my-8 max-w-5xl">
 			<div className="bg-gray-800/50 rounded-md shadow overflow-hidden border-[1px] border-gray-700">
 				{children}
 			</div>
